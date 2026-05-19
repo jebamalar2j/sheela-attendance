d92efa6ffb5f4881fe5a1a606eadab2ad5ba4903
unknown <jebamalar2j@gmail.com>
2026-05-19 16:01:11 +0530

    Sheela Physiocare attendance app

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'sheela-v2';
+const FILES = ['./', './index.html', './manifest.json', './logo_white.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('script.google.com')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)));
+});
